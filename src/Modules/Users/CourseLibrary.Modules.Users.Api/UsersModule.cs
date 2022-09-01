8766754497c5f38a2f8a4a92e6ac47ab7bd4fc03
ktutak1337 <ktutak1337@gmail.com>
2022-09-01 09:25:25 +0200

    Add endpoints to users module (Code)

--- src/Modules/Users/CourseLibrary.Modules.Users.Api/UsersModule.cs
+++ src/Modules/Users/CourseLibrary.Modules.Users.Api/UsersModule.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CourseLibrary.Shared.Abstractions.Modules;
 using CourseLibrary.Modules.Users.Core;
+using CourseLibrary.Shared.Abstractions.Dispatchers;
+using CourseLibrary.Modules.Users.Core.Queries;
+using CourseLibrary.Modules.Users.Core.Commands;
+using CourseLibrary.Modules.Users.Core.Services;
 
 namespace CourseLibrary.Modules.Users.Api;
 
@@ -30,5 +34,33 @@ internal sealed class UsersModule : IModule
     public void Expose(IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet("/users", () => "Users API!").WithTags("Users").WithName("Users entry point");
+
+        endpoints.MapGet("/users/{id:guid}", async (Guid id, IDispatcher dispatcher) =>
+        {
+            var user = await dispatcher.QueryAsync(new GetUser {UserId = id});
+            return user is null ? Results.NotFound() : Results.Ok(user);
+        }).WithTags("Users").WithName("Get user");
+
+        endpoints.MapGet("/me", async (IDispatcher dispatcher, HttpContext context) =>
+        {
+            var user = await dispatcher.QueryAsync(new GetUser {UserId = UserId(context)});
+            return user is null ? Results.NotFound() : Results.Ok(user);
+        }).RequireAuthorization().WithTags("Account").WithName("Get account");
+
+        endpoints.MapPost("/sign-up", async (SignUp command, IDispatcher dispatcher) =>
+        {
+            await dispatcher.SendAsync(command with {UserId = Guid.NewGuid()});
+            return Results.NoContent();
+        }).WithTags("Account").WithName("Sign up");
+
+        endpoints.MapPost("/sign-in", async (SignIn command, IDispatcher dispatcher, ITokenStorage storage) =>
+        {
+            await dispatcher.SendAsync(command);
+            var jwt = storage.Get();
+            return Results.Ok(jwt);
+        }).WithTags("Account").WithName("Sign in");
+
+        static Guid UserId(HttpContext context)
+            => string.IsNullOrWhiteSpace(context.User.Identity?.Name) ? Guid.Empty : Guid.Parse(context.User.Identity.Name);
     }
 }
